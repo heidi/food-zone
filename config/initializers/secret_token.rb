334b479c236571cacf93aace7260ed9995efae9c
Heidi Galbraith <heidi@verticalbrands.com>
2013-07-25 19:35:19 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodZone::Application.config.secret_token = '596b5a27c5cad760632259e9d577f5f6a78f7d7ec4957ec3ecd6d5610fc120fc266c6ca7accadf2ff75a463fa0bf9fa70d391ebc919a788c4d5839c21478b0bf'
