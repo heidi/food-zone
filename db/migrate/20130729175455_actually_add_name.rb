69deb7aff5aad1b68ed9cec338cac65dfe38f421
Heidi Galbraith <heidi@verticalbrands.com>
2013-07-29 11:02:53 -0700

    this time for sure

--- db/migrate/20130729175455_actually_add_name.rb
+++ db/migrate/20130729175455_actually_add_name.rb
@@ -1,6 +1,6 @@
 class ActuallyAddName < ActiveRecord::Migration
   def change
     add_column :admin_users, :first_name, :string
-    add_column :admin_users, :last_name, :st
+    add_column :admin_users, :last_name, :string
   end
 end
