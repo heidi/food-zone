7b0b198dd27c91778cf815974605b6889dd40dff
Heidi Galbraith <heidi@verticalbrands.com>
2013-07-29 10:59:37 -0700

    fix name adding migration

--- db/migrate/20130729175455_actually_add_name.rb
+++ db/migrate/20130729175455_actually_add_name.rb
@@ -1,4 +1,4 @@
-class AddNameToAdminUsers < ActiveRecord::Migration
+class ActuallyAddName < ActiveRecord::Migration
   def change
     add_column :admin_users, :first_name, :string
     add_column :admin_users, :last_name, :st
